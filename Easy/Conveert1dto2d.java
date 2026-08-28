0964401e2da468b81f4e2017e997b341087cf276
yamuna314 <elumalaiyamuna21@gmail.com>
2026-08-28 18:59:49 +0530

    Day 4:Convert 1D TO 2D

---
+++ Easy/Conveert1dto2d.java
@@ -0,0 +1,23 @@
+public class Conveert1dto2d {
+    public int[][] construct2DArray(int[] original, int m, int n) {
+
+        if (original.length != m * n) {
+            return new int[0][0];
+        }
+
+        int[][] result = new int[m][n];
+
+        int k = 0;
+
+        for (int i = 0; i < m; i++) {
+            for (int j = 0; j < n; j++) {
+                result[i][j] = original[k];
+                k++;
+            }
+        }
+
+        return result;
+    }
+}
+    
+
