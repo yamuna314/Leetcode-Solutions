f7e80645e2259286f8880d3aa563a78ca19c3189
yamuna314 <elumalaiyamuna21@gmail.com>
2026-09-07 21:35:22 +0530

    Day 11:Reversse Vowels of a String

---
+++ Easy/ReverseVowelsofaString.java
@@ -0,0 +1,36 @@
+public class ReverseVowelsofaString {
+    public String reverseVowels(String s) {
+        char[] arr = s.toCharArray();
+
+        int left = 0;
+        int right = arr.length - 1;
+
+        while (left < right) {
+
+            while (left < right && !isVowel(arr[left])) {
+                left++;
+            }
+
+            while (left < right && !isVowel(arr[right])) {
+                right--;
+            }
+
+            char temp = arr[left];
+            arr[left] = arr[right];
+            arr[right] = temp;
+
+            left++;
+            right--;
+        }
+
+        return new String(arr);
+    }
+
+    public boolean isVowel(char c) {
+        return c == 'a' || c == 'e' || c == 'i' ||
+               c == 'o' || c == 'u' ||
+               c == 'A' || c == 'E' || c == 'I' ||
+               c == 'O' || c == 'U';
+    }
+    
+}
